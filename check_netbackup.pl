e0b85fcc635508a095152706c270924fbfa482d0
aramilialon <giorgio@maggiolo.net>
2017-04-04 16:06:21 +0200

    All the operations completed. Still missing the help

--- check_netbackup.pl
+++ check_netbackup.pl
@@ -27,7 +27,7 @@ GetOptions (
 	"stype=s" => \my $stype,					# for diskpools
 	"diskpool=s" => \my $diskpool,				# for diskpools
 	"tapelibrary=i" => \my $tapelibrary,		# for tapelibrary
-	"policy=s" => \my $policyname,				# for backupcheck
+	"policyname=s" => \my $policyname,				# for backupcheck
 	"backupnumber=i" => \my $backupnumber,		# for backupcheck
 ) or Error("$0: Error in command line arguments\n");
 
@@ -38,11 +38,13 @@ sub Error {
 
 my $output;
 
+
+
 sub check_diskpools{
 	Error('Option --warning required') unless $warning;
 	Error('Option --critical required') unless $critical;
 	Error('Option --diskpool required') unless $diskpool;
-	if ($warning > $critical){
+	if ($warning < $critical){
 		my $temp = $warning;
 		$warning = $critical;
 		$critical = $temp; 
@@ -73,29 +75,75 @@ sub check_diskpools{
 }
 
 
+sub check_drive_status{
+	Error('Option --tapelibrary required') unless ($tapelibrary || $tapelibrary eq "0");
+	Error('Option --warning required') unless $warning;
+	Error('Option --critical required') unless $critical;
+	if ($warning < $critical){
+		my $temp = $warning;
+		$warning = $critical;
+		$critical = $temp; 
+	}
+	eval {
+		$output = `vmquery -rn $tapelibrary -bx | findstr -i scratch | find /v /c \"\"`;
+	};
+	if ($@) {
+		print "UNKNOWN: Something went wrong - $@\n";
+		exit(3);
+	}
+	$output =~ /(.*)\n/;
+	$output = $1;
+	if($output > $warning ){
+		print "OK: There are $output scratch cassettes in the $tapelibrary tape library\n";
+		exit(0);
+	} elsif($output < $warning and $output > $critical){
+		print "WARNING: There are $output scratch cassettes in the $tapelibrary tape library\n";
+		exit(1);
+	} else {
+		print "CRITICAL: There are $output scratch cassettes in the $tapelibrary tape library\n";
+		exit(2);
+	}
+}
 
-# sub check_drive_status{
-# 	Error('Option --tapelibrary required') unless $tapelibrary;
-# 	eval {
-# 		$output = capture("perl plugins/vminfo.pl --vmname ".ucfirst(lc($old_vm_name))." --username $username --password $password --server $vc_server --fields overallStatus");
-# 	};
-# 	if ($@) {
-# 		print "UNKNOWN: Something went wrong - $@\n";
-# 		exit(3);
-# 	}
-# }
+sub check_backup{
+	Error('Option --policyname required') unless $policyname;
+	Error('Option --backupnumber required') unless $backupnumber;
+	eval {
+		$output = `bpimagelist -policy \"$policyname\" -hoursago 24 -U`;
+	};
+	if ($@) {
+		print "UNKNOWN: Something went wrong - $@\n";
+		exit(3);
+	}
+	my $counter = 0;
+	# my $counter = ($output =~ tr/($policyname)//);
+	while ($output =~ /$policyname/g) { $counter++; }
+	if($counter == $backupnumber){
+		print "OK: the task $policyname has $counter backups\n";
+		exit(0);
+	} else {
+		print "WARNING: the task $policyname has $counter backups (requested $backupnumber)\n";
+		exit(1);
+	}
+}
 
-# sub check_backup{
-# 	Error('Option --policyname required') unless $policyname;
-# 	Error('Option --backupnumber required') unless $backupnumber;
-# 	eval {
-# 		$output = capture("perl plugins/vminfo.pl --vmname ".ucfirst(lc($old_vm_name))." --username $username --password $password --server $vc_server --fields overallStatus");
-# 	};
-# 	if ($@) {
-# 		print "UNKNOWN: Something went wrong - $@\n";
-# 		exit(3);
-# 	}
-# }
+sub check_down_drive {
+	eval {
+		$output = `vmoprcmd -devmon ds`;
+	};
+	if ($@) {
+		print "UNKNOWN: Something went wrong - $@\n";
+		exit(3);
+	}
+	foreach my $line ($output){
+		if ($line =~ /down/i){
+			print "CRITICAL: There is a down drive\n";
+			exit(2);
+		}
+	}
+	print "OK: there isn't any down drive\n";
+	exit(0);
+}
 
 Error('Option --operation required') unless $operation;
 
@@ -103,6 +151,8 @@ if ($operation eq "diskpools"){
 	check_diskpools();
 } elsif ($operation eq "tapelibrary"){
 	check_drive_status();
+} elsif ($operation eq "downdrive"){
+	check_down_drive();
 } elsif ($operation eq "backupcheck"){
 	check_backup();
 } else {
